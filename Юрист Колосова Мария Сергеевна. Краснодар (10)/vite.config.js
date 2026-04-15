7bfe0868401c28c0b2112e817c4b2d18356cf0bb
infoforkate-gif <infoforkate@gmail.com>
2026-04-16 00:30:04 +0300

    Update vite.config.js

--- Юрист Колосова Мария Сергеевна. Краснодар (10)/vite.config.js
+++ Юрист Колосова Мария Сергеевна. Краснодар (10)/vite.config.js
@@ -1,10 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import path from "path"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-})
+  base: './',
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
